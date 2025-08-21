d8218784abc53abd004911dd33fac18d9ed5bb91
MQ-579 <mmamaqueen@gmail.com>
2025-08-21 12:51:40 +0100

    feat: add basic Spring Boot project setup

---
+++ src/main/java/com/queenmmama/safeguarding/safeguarding_api/SafeguardingApiApplication.java
@@ -0,0 +1,13 @@
+package com.queenmmama.safeguarding.safeguarding_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeguardingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeguardingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queenmmama/safeguarding/safeguarding_api/SafeguardingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queenmmama.safeguarding.safeguarding_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeguardingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f9b3f511080fa1d083ab6664804f5d8bb6f36224
MQ-579 <mmamaqueen@gmail.com>
2025-09-01 17:24:02 +0100

    Added Javadoc for SafeguardingAPIApplication class

--- src/main/java/com/queenmmama/safeguarding/safeguarding_api/SafeguardingApiApplication.java
+++ src/main/java/com/queenmmama/safeguarding/safeguarding_api/SafeguardingApiApplication.java
@@ -3,8 +3,20 @@ package com.queenmmama.safeguarding.safeguarding_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the Safeguarding API Spring Boot application.
+ * 
+ * <p>This class contains the main method that bootstraps the application by initialising Spring Boot's auto-configuration and component scanning feature, and starting the embedded server.</p>
+ * 
+ */
 @SpringBootApplication
 public class SafeguardingApiApplication {
+
+    /**
+     * Main method that serves as the entry point for the Spring Boot application.
+     * @param args command-line arguments (not used)
+     * 
+     */
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafeguardingApiApplication.class, args);
